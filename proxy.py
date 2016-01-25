6d4e8b0e2c26a1a95a83fe53dba0d7bde3ff3bae
KJ <kernel1983@gmail.com>
2016-01-25 13:16:14 +0800

    clean up

--- proxy.py
+++ proxy.py
@@ -92,54 +92,21 @@ class LocalConnection(object):
         self.upstream_cls = upstream_cls
         self.stream.set_close_callback(self.on_connection_close)
         self.dest = None
-        self.sent_reply = False
         self.on_connected()
 
     def on_connected(self):
         logger.debug('start connect...')
-        # self.stream.read_bytes(2, self.on_auth_num_methods)
         self.cmd = 0x01
         self.atyp = 0x03
         self.domain_name = self.addr[0]
         self.raw_dest_addr = struct.pack("!B", len(self.addr[0])) + self.addr[0]
         self.raw_dest_port = struct.pack("!H", self.addr[1])
         self.dest = self.addr
-        # print repr(self.raw_dest_addr), repr(self.raw_dest_port)
-
-        # self.on_request_cmd_address_type()
         self.do_connect()
 
     def on_connection_close(self):
         logger.debug("disconnected!")
         self.clean_upstream()
-
-    # def on_auth_num_methods(self, data):
-    #     # ver + nmethods. ref: rfc1928. P3
-    #     self.ver, self.auth_nmethods = struct.unpack("!BB", data)
-    #     logger.debug("version: 0x%X, number of methods: %d" %
-    #                  (self.ver, self.auth_nmethods))
-    #     if self.ver != 0x05:
-    #         logger.warning("version mismatch, closing connection!")
-    #         self.stream.close()
-    #     else:
-    #         self.stream.read_bytes(self.auth_nmethods, self.on_auth_methods)
-    #
-    # def on_auth_methods(self, data):
-    #     self.auth_methods = struct.unpack("B" * self.auth_nmethods, data)
-    #     if 0x00 in self.auth_methods:
-    #         self.stream.write(struct.pack("!BB", self.ver, 0x00))
-    #         self.on_auth_finished()
-    #     else:
-    #         logger.warning("the client does not support \"no authentication\", \
-    #             closing connection!")
-    #         self.stream.close()
-    #
-    # def on_auth_finished(self):
-    #     logger.debug("authentication finished.")
-    #     self.wait_request()
-    #
-    # def wait_request(self):
-    #     self.stream.read_bytes(4, self.on_request_cmd_address_type)
 
     def wait_for_domain_name(self):
         self.raw_dest_addr = ""
@@ -160,85 +127,15 @@ class LocalConnection(object):
         self.dest_addr = self.domain_name
         self.wait_destination_port()
 
-    # def on_request_cmd_address_type(self, data=None):
-    #     _ver, self.cmd, _rsv, self.atyp = struct.unpack("!BBBB", data)
-    #     logger.debug("command: %s, address type: %s" % (
-    #         self.COMMAND_MAP.get(self.cmd, "UNKNOWN COMMAND"),
-    #         self.ADDRESS_TYPE_MAP.get(self.atyp, "UNKNOWN ADDRESS TYPE")))
-    #
-    #     if self.cmd not in self.ACCEPTED_COMMANDS:
-    #         self.write_reply(0x07)
-    #         self.stream.close()
-    #
-    #     if self.atyp not in self.ACCEPTED_ADDRESS_TYPES:
-    #         self.write_reply(0x08)
-    #         self.stream.close()
-    #
-    #     if self.atyp in self.ADDRESS_TYPE_LENGTH:
-    #         self.stream.read_bytes(self.ADDRESS_TYPE_LENGTH[self.atyp],
-    #                                self.on_request_fixed_length_address)
-    #     else:
-    #         self.wait_for_domain_name()
-
-    # def wait_destination_port(self):
-    #     self.stream.read_bytes(2, self.on_destination_port)
-    #
-    # def convert_readable_address(self, addr):
-    #     return socket.inet_ntop(socket.AF_INET if self.atyp == 0x01
-    #                             else socket.AF_INET6, addr)
-    #
-    # def convert_machine_address(self, addr_type, addr):
-    #     return socket.inet_pton(addr_type, addr)
-    #
-    # def on_request_fixed_length_address(self, data):
-    #     self.raw_dest_addr = data
-    #     self.dest_addr = self.convert_readable_address(data)
-    #     logger.debug("received address: %s" % self.dest_addr)
-    #     self.wait_destination_port()
-    #
-    # def on_destination_port(self, data):
-    #     self.raw_dest_port = data
-    #     port, = struct.unpack("!H", data)
-    #     self.dest = (self.dest_addr, port)
-    #     logger.debug("received port: %s" % port)
-    #     self.on_request_received()
-    #
-    # def on_request_received(self):
-    #     logger.debug("received request: " + self.COMMAND_MAP[
-    #         self.cmd] + ", destination: %s:%d" % self.dest)
-    #
-    #     self.command_processors = {
-    #         0x01:   self.do_connect,
-    #     }
-    #     self.command_processors[self.cmd]()
-
-    # def do_connect(self):
-    #
-    #     self.upstream = self.upstream_cls(self.dest,
-    #                                 socket.AF_INET6 if self.atyp == 0x04 else socket.AF_INET,
-    #                                 self.on_upstream_connect, self.on_upstream_error,
-    #                                 self.on_upstream_data, self.on_upstream_close)
-
     def do_connect(self):
         config = Config.current()
 
         logger.debug("server : %s, %s" % (config.server, config.server_port))
-        # logger.debug("server dest: %s, %s" % self.dest)
+        logger.debug("server dest: %s, %s" % self.dest)
         dest = (config.server, config.server_port)
         self.upstream = self.upstream_cls(dest, socket.AF_INET,
                     self.on_upstream_connect, self.on_upstream_error,
                     self.on_upstream_data, self.on_upstream_close)
-
-
-    # def on_upstream_connect(self, _dummy):
-    #     addr_type = self.upstream.address_type
-    #     addr = self.upstream.address
-    #     logger.debug("upstream connected from %s:%d" % addr)
-    #     src_addr = self.convert_machine_address(addr_type, addr[0])
-    #     self.write_reply(0x00, src_addr + struct.pack("!H", addr[1]))
-    #
-    #     on_finish = functools.partial(self.on_socks_data, finished=True)
-    #     self.stream.read_until_close(on_finish, self.on_socks_data)
 
     # def client_close(self, data=None):
     #     print data
@@ -254,8 +151,6 @@ class LocalConnection(object):
 
     def on_upstream_connect(self, _dummy):
         config = Config.current()
-        self.write_reply(0x00, socket.inet_aton('0.0.0.0')
-                         + struct.pack("!H", config.server_port))
         self.write_request()
         on_finish = functools.partial(self.on_socks_data, finished=True)
         self.stream.read_until_close(on_finish, self.on_socks_data)
@@ -275,11 +170,8 @@ class LocalConnection(object):
                 address_type = 0x01
         self.upstream.write(struct.pack("!B", address_type) + data)
 
-
     def on_upstream_error(self, _dummy, no):
         logger.debug("upstream error: %s" % no)
-        if not self.sent_reply:
-            self.write_reply(self.ERRNO_MAP.get(no, 0x01))
         self.stream.close()
 
     def on_upstream_data(self, _dummy, data, finished=False):
@@ -312,36 +204,14 @@ class LocalConnection(object):
             logger.debug("sent %d bytes of data to upstream." %
                          len(data))
 
-    def write_reply(self, code, data=None):
-        # address_type = self.atyp
-        # if data is None:
-        #     if self.dest:
-        #         data = self.raw_dest_addr + self.raw_dest_port
-        #     else:
-        #         data = struct.pack("!BLH", 0x01, 0x00, 0x00)
-        # else:
-        #     if self.atyp == 0x03:
-        #         address_type = 0x01
-        # self.stream.write(struct.pack("!BBBB", self.ver, code, 0x00, address_type
-        #                               ) + data)
-        # logger.debug("sent reply: %s" % (self.REPLY_CODES.get(
-        #     code, "UNKNOWN REPLY")))
-        self.sent_reply = True
-
-
-
-
-
 def get_proxy(url):
     url_parsed = urlparse(url, scheme='http')
     proxy_key = '%s_proxy' % url_parsed.scheme
     return os.environ.get(proxy_key)
 
-
 def parse_proxy(proxy):
     proxy_parsed = urlparse(proxy, scheme='http')
     return proxy_parsed.hostname, proxy_parsed.port
-
 
 def fetch_request(url, callback, **kwargs):
     proxy = get_proxy(url)
@@ -356,7 +226,6 @@ def fetch_request(url, callback, **kwargs):
     req = tornado.httpclient.HTTPRequest(url, **kwargs)
     client = tornado.httpclient.AsyncHTTPClient()
     client.fetch(req, callback, raise_error=False)
-
 
 class ProxyHandler(tornado.web.RequestHandler):
     SUPPORTED_METHODS = ['GET', 'POST', 'CONNECT']
@@ -414,14 +283,6 @@ class ProxyHandler(tornado.web.RequestHandler):
     def post(self):
         return self.get()
 
-    # def read_from_upstream(self, data):
-    #     print "read_from_upstream", data
-    #     self.client.write(data)
-    #     # client.write(b"HTTP/1.0 407 Proxy Authentication Required\r\n")
-    #     # client.write(b"Proxy-Authenticate: Basic\r\n\r\n")
-    #     # client.close()
-
-
     @tornado.web.asynchronous
     def connect(self):
         # print self.request.method, self.request.headers.items()
@@ -433,25 +294,17 @@ class ProxyHandler(tornado.web.RequestHandler):
 
 
 if __name__ == '__main__':
-    # pardir = os.path.join(
-        # os.path.abspath(os.path.join(os.path.dirname(__file__))), os.pardir)
-    # sys.path.insert(0, pardir)
-
     config_path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__))), 'config', 'config.json')
     config = Config.current(config_path)
     crypto.setup_table(config.password, config.method)
 
-    port = config.local_port
-    # if len(sys.argv) > 1:
-    #     port = int(sys.argv[1])
-
-    # print ("Starting HTTP proxy on port %d" % port)
     from fukei.utils import log_config
     log_config('FukeiLocal', config.debug)
-    # run_proxy(port)
+
     app = tornado.web.Application([
         (r'.*', ProxyHandler),
     ], debug=config.debug)
 
-    app.listen(port)
+    app.listen(config.local_port)
+    # print ("Starting HTTP proxy on port %d" % config.local_port)
     tornado.ioloop.IOLoop.instance().start()
